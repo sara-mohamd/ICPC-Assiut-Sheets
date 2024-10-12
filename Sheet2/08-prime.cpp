daceecc2c3cf1b57e2e9da3bebaf393c82395b42
sara-mohamd <saraamohamed0038@gmail.com>
2024-10-12 20:29:05 +0300

    problem 8

---
+++ Sheet2/08-prime.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+  int number;
+  bool flag = 1;
+  cin >> number;
+  // numbers less than 2 is not prime
+  if (number < 2)
+    flag = 0;
+  else
+  {
+    for (int i = 2; i < sqrt(number); i++)
+    {
+      if (!(number % i))
+      {
+        flag = 0;
+        break;
+      }
+    }
+  }
+}
