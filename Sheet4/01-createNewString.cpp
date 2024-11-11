5693816fe47f5429eb208a9ae53893f748ddd0f7
sara-mohamd <saraamohamed0038@gmail.com>
2024-11-11 19:23:47 +0200

    problem 1

---
+++ Sheet4/01-createNewString.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+  string s, t;
+  cin >> s >> t;
+
+  cout << s.length() << " " << t.length() << endl
+       << s << " " << t;
+  return 0;
+}
