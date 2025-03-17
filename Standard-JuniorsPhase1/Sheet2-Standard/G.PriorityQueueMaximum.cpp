ac5b8286c054882996913c4b5b1fd8e5d45f46e4
sara-mohamd <saraamohamed0038@gmail.com>
2025-03-17 11:58:30 +0200

    upsolve G

---
+++ Standard-JuniorsPhase1/Sheet2-Standard/G.PriorityQueueMaximum.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  int n, x;
+  cin >> n;
+  priority_queue<int> pq;
+  string s;
+  while (n--)
+  {
+    cin >> s;
+    if (s == "push")
+    {
+      cin >> x;
+      pq.push(x);
+    }
+    if (s == "pop")
+    {
+      pq.pop();
+    }
+    if (s == "top")
+    {
+      cout << pq.top() << endl;
+    }
+  }
+}
