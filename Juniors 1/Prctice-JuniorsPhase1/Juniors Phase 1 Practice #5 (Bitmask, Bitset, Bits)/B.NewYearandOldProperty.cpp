d42dd834111731c12e7dd3361a933b8b5aadc5de
sara-mohamd <saraamohamed0038@gmail.com>
2025-08-26 21:56:57 +0300

    Add Solution of B

---
+++ Juniors 1/Prctice-JuniorsPhase1/Juniors Phase 1 Practice #5 (Bitmask, Bitset, Bits)/B.NewYearandOldProperty.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+void fast() { ios_base::sync_with_stdio(false), cin.tie(NULL); }
+
+signed main()
+{
+  fast();
+  int r1, r2, answer = 0;
+  cin >> r1 >> r2;
+  int x = 0;
+  for (int bit = 0; bit < 63; bit++)
+  {
+    x += (1LL << bit);
+    for (int i = 0; i < bit; i++)
+    {
+      int num = x - (1LL << i);
+      if (num >= r1 && num <= r2)
+        answer++;
+    }
+  }
+
+  cout << answer << '\n';
+}
