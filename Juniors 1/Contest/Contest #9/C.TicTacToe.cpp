777e2d8230c13a5427436e6277fed07013f3c1c0
sara-mohamd <saraamohamed0038@gmail.com>
2025-09-19 16:15:22 +0300

    Add: solution of problem C

---
+++ Juniors 1/Contest/Contest #9/C.TicTacToe.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+void fast() { ios::sync_with_stdio(false), cin.tie(NULL); }
+
+signed main()
+{
+  fast();
+  int n;
+  cin >> n;
+  cout << "draw\n";
+}
